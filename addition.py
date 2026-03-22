1d182540d836aa7d50aee7ba1ef9ccb17d14c604
Ranibalina <ranibalina1@gmail.com>
2026-03-22 14:13:56 -0400

    addition.py

---
+++ addition.py
@@ -0,0 +1,14 @@
+def addition(num1,num2):
+ return(num1+num2)
+
+print(num1:=int(input("enter first number:")))
+print(num2:=int(input("enter second number:")))
+
+result=addition(num1,num2)
+print(result)
+
+
+
+
+
+
